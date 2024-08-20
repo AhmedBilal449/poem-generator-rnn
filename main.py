1ecd37ddb2d8ffe318bcfbda03d0fb17ae1af785
Bilal <ahmed.bilal.work@gmail.com>
2024-08-20 01:57:19 +0100

    Added libraries to the file

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print ("hello")
+import random
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from keras.models import Sequential
+from keras.layers import LSTM, Dense, Activation
+from keras.optimizers import RMSprop
+
